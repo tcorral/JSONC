5eae292d44625d0dc243b6ae5fbadadb2989d9ba
karlr42 <karlr42@gmail.com>
2014-10-10 13:52:49 +0000

    Remove a line-ending substitution that can lead to corruption of data, causing random failures in decompression of such data.

--- vendor/base64.js
+++ vendor/base64.js
@@ -83,7 +83,6 @@ var Base64 = {
 
   // private method for UTF-8 encoding
   _utf8_encode : function (string) {
-    string = string.replace(/\r\n/g,"\n");
     var utftext = "";
 
     for (var n = 0; n < string.length; n++) {
